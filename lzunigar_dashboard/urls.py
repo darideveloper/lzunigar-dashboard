cbc48c3d3ae2a39babe72677b2dbb116f2115761
darideveloper <darideveloper@gmail.com>
2024-04-13 16:54:54 -0600

    feat (views): redirect to admin

--- lzunigar_dashboard/urls.py
+++ lzunigar_dashboard/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin/')),
     path('admin/', admin.site.urls),
     path('contact-form/', include('contact_forms.urls')),
 ]
